a19f2611a0b05c6702f0868a2aa6fdf89d2cf1b2
Shayan <themegabyte@users.noreply.github.com>
2022-01-23 14:45:30 -0600

    Fixed formatiing and moved to jsdeliver

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,9 @@
-javascript:(function () { 
-    var s = document.createElement('script');
-    s.setAttribute("type", "text/javascript");
-    s.setAttribute("src", "https://rawgit.com/Sjord/tablefilter/master/tablefilter.js");
-    document.body.appendChild(s);
+javascript: (function () {
+  var s = document.createElement("script");
+  s.setAttribute("type", "text/javascript");
+  s.setAttribute(
+    "src",
+    "https://cdn.jsdelivr.net/gh/themegabyte/tablefilter@master/tablefilter.js"
+  );
+  document.body.appendChild(s);
 })();
